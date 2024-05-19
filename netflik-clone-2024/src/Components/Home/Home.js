fd5674eb2ee30e9b0247a1a26b31bb8382fbd6cf
abdlhak-code <156803374+abdlhak-code@users.noreply.github.com>
2024-05-19 03:57:47 -0700

    axios and requests

--- netflik-clone-2024/src/Components/Home/Home.js
+++ netflik-clone-2024/src/Components/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import RowList from '../Rows/RowList/RowList'
+import Banner from '../Banner/Banner'
 function Home() {
   return (
     <>
     <Header />
+    <Banner/>
+    <RowList/>
     <Footer />
     </>
   )
